ce26d65d4fac704efcd056895801ca9b31378101
Stefan Alin <stefanalindascalescu@outlook.com>
2019-06-23 13:49:28 +0300

    Added the last part of the homework

--- app.js
+++ app.js
@@ -13,14 +13,42 @@ var num1 =  9;
 var num2 = 12;
 
 
-
-
 while (num2 > 0) {
     var reminder = num1 % num2;
 	num1 = num2;
 	num2 = reminder;
 }
 
-console.log("al mai mare divizor " + Number(num1));
+console.log("al mai mare divizor " + (num1));
+
+// Converise celsius Fahrenheit si viceversa
 
 
+var temp = Number(prompt("Temperatura este de?: "));
+var unit = prompt("Unitatea de masura C sau F: ");
+
+// if(unit === "C"){
+// 	//calcul Fahrenheit
+// 	var fahrenheit = (temp * 1.8) + 32;
+// 	alert("Afara sunt " + fahrenheit + " grade Fahrenheit");
+// }else if(unit === "F"){
+// 	//calcul celsius
+// 	var celsius = (temp - 32) / 1.8;
+// 	alert("Afara sunt " + celsius + " grade Celsius");
+// }else{
+// 	alert("Temperatura sau unitatea de masura folosita nu este valabila");
+// }
+
+
+switch (unit) {
+	case "C":
+		var fahrenheit = (temp * 1.8) + 32;
+		alert("Afara sunt " + fahrenheit + " grade Fahrenheit");
+		break;
+	case "F":
+		var celsius = (temp - 32) / 1.8;
+		alert("Afara sunt " + celsius + " grade Celsius");
+		break;
+	default:
+		alert(unit + " Nu este o unitate de masura valabila");
+}
